2051ce1c62de9aa3d19b1c1731306d6fe6c255a3
jao321 <50146651+jao321@users.noreply.github.com>
2020-11-22 14:31:27 -0300

    Add files via upload

---
+++ YClon.py
@@ -0,0 +1,199 @@
+import os
+import time
+import sys
+import platform
+from alive_progress import alive_bar
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import AgglomerativeClustering
+import hdbscan
+import sparse_dot_topn.sparse_dot_topn as ct
+
+
+def directory_path(file_path):
+  OS = platform.system()
+  if OS != "Windows":
+    temp =  file_path.split(os.sep)
+    file_path = file_path.replace(temp[len(temp)-1],"")
+    return(file_path)
+
+def build_kmers_tf_idf(sequence, ksize=3): 
+    ngrams = zip(*[sequence[i:] for i in range(ksize)])
+    return [''.join(ngram) for ngram in ngrams]
+
+
+filename =  sys.argv[1]
+method = "AHAM"
+thr = 0.09
+
+
+for x in range(1,len(sys.argv)):
+  if sys.argv[x].find("--input") != -1:
+    filename = sys.argv[x+1]
+  elif sys.argv[x].find("--method") != -1: 
+    method = sys.argv[x+1]
+  elif sys.argv[x].find("--thr") != -1:
+    thr = float(sys.argv[x+1])
+
+
+
+
+start_time = time.time()
+
+
+
+path = directory_path(filename)
+
+
+f = open(filename, 'r')
+i=0
+fail = 0
+clonotypes = {}
+file_size = 0
+print("Processing file")
+for x in f:
+  file_size +=1
+  if  x.find ("Processed") == -1 and x.find("sequence_id") == -1:
+    x = x.strip()
+    data = x.split("\t") 
+    try:
+      cdr3len = str(len(data[junc_indx]))
+      vGene = data[vGene_indx].split('*') #include all v gene alleles
+      jGene = data[jGene_indx].split('*') #include all v gene alleles
+      key = vGene[0]+","+jGene[0]+","+cdr3len
+      clonotypes.setdefault(key, [])
+      proclonotype = [data[seq_id_indx],data[junc_indx]]
+      clonotypes[key].append(proclonotype)
+    except:
+      fail +=1
+  else:
+    x = x.strip()
+    data = x.split("\t")
+
+    seq_id_indx = data.index("sequence_id")
+    try:
+      junc_indx = data.index("cdr3")
+    except:
+      print("There is no column named cdr3")
+    vGene_indx = data.index("v_call")
+    jGene_indx = data.index("j_call")
+    colunas = [data[seq_id_indx],data[junc_indx]]
+    # print(colunas)
+
+
+
+
+
+
+  
+f.close()
+temp_filename = path+"YClon_temp.txt"
+size = len(clonotypes)
+temp = open(temp_filename, 'w')
+count = 0
+total_clust = 0
+unico_pq_VJLen = 0
+maior = 0
+a = 0
+with alive_bar(len(clonotypes), title="Clonotyping") as bar: 
+  for key in clonotypes: #each key is the combination of V gene, J gene and the length of cdr3, the values are the sequence ID and cdr3 sequence
+    bar()
+    if len(clonotypes[key]) > 1:
+      teste = pd.DataFrame(clonotypes[key])
+      teste.columns = colunas
+
+      junc_seq = teste['cdr3']
+      seq_id = teste['sequence_id']
+
+      vectorizer = TfidfVectorizer(min_df=1, analyzer=build_kmers_tf_idf)
+      tf_idf_matrix = vectorizer.fit_transform(junc_seq)  
+      dist = 1 - cosine_similarity(tf_idf_matrix)
+      if method ==  "AHAM":
+        clusterer = AgglomerativeClustering(distance_threshold = thr, n_clusters= None, affinity="precomputed",linkage='average')
+      elif method == "HDBSCAN":
+        clusterer = hdbscan.HDBSCAN(min_cluster_size=3, cluster_selection_epsilon= thr, metric='precomputed')
+      cluster_teste = clusterer.fit(dist)
+      clone = cluster_teste.labels_
+      # if cluster_teste.labels_.max() != -1:
+      maior = count + cluster_teste.labels_.max() + 1
+      for i in range(0, len(clonotypes[key])):
+        if clone[i] != -1:
+          clone_id = count + int(clone[i]) +1
+        else:
+          clone_id = maior + 1
+          maior += 1
+          total_clust += 1
+        
+        temp.write(str(seq_id[i])+","+str(clone_id)+"\n")	
+      count = maior
+
+
+      total_clust += cluster_teste.labels_.max() + 1	
+
+    else:
+      unico_pq_VJLen +=1
+      count += 1
+      total_clust += 1
+      teste = pd.DataFrame(clonotypes[key])
+      teste.columns = colunas
+      seq_id = teste['sequence_id']
+      for i in range(0, len(clonotypes[key])):
+        temp.write(str(seq_id[i])+","+str(count)+"\n")
+    
+
+
+
+
+
+		
+
+in_temp = open(temp_filename, 'r')
+in_airr = open(filename, 'r')
+out_filename = filename.replace(".tsv","_YClon_clonotyped.tsv")
+out = open(out_filename, 'w+')
+clonotipo = {}
+maior ={}
+maximo = 0
+
+
+for x in in_temp:
+	data = x.split(",")
+	clonotipo[data[0]] = data[1]
+	if data[1] not in maior:
+		maior[data[1]] = list(data[0])
+	else:
+		maior[data[1]].append(data[0])
+for i in maior:
+	if maximo < len(maior[i]):
+		maximo = len(maior[i])
+# print(maximo)
+
+
+# with alive_bar(file_size, title="Writing output file") as bar: 
+with alive_bar(file_size, title="Writing output file") as bar:
+  for x in in_airr:
+    bar()
+    if x.find("sequence_id") == -1:
+      data = x.split('\t')
+      if data[0] in clonotipo:
+        for i in range(0, len(data)):
+          out.write(data[i].strip()+"\t")
+          out.write(clonotipo[data[0]])
+    else:
+      out.write(x.strip()+"\t"+"clone_id\n")
+
+
+
+os.remove(temp_filename)
+
+
+
+
+current_time = time.time()
+elapsed_time = current_time - start_time
+
+
+print("Terminou iteração em: " + "%.3f" % int(elapsed_time))
